9abc485e7fc972de096e7d5d58d76e0047c68d9a
czinner <shon.czinner0@gmail.com>
2025-06-16 21:23:21 -0400

    added touch events

--- draw.js
+++ draw.js
@@ -24,25 +24,40 @@ export function enableCanvasDrawing(canvas, drawDots = false) {
     }
   }
 
-  canvas.addEventListener("mousedown", (e) => {
+  function getCanvasCoords(e) {
+    const rect = canvas.getBoundingClientRect();
+    let x, y;
+    if (e.touches) {
+      x = e.touches[0].clientX - rect.left;
+      y = e.touches[0].clientY - rect.top;
+    } else {
+      x = e.offsetX;
+      y = e.offsetY;
+    }
+    return { x, y };
+  }
+
+  function startDrawing(e) {
+    e.preventDefault();
+    const { x, y } = getCanvasCoords(e);
     isDrawing = true;
-    lastX = e.offsetX;
-    lastY = e.offsetY;
+    lastX = x;
+    lastY = y;
     if (!drawDots) ctx.beginPath();
     recordPoint(0);
-
     samplingInterval = setInterval(() => {
       recordPoint(0);
     }, 20);
-  });
+  }
 
-  canvas.addEventListener("mousemove", (e) => {
+  function moveDrawing(e) {
     if (!isDrawing) return;
-    lastX = e.offsetX;
-    lastY = e.offsetY;
-  });
+    const { x, y } = getCanvasCoords(e);
+    lastX = x;
+    lastY = y;
+  }
 
-  function stopDrawing() {
+  function stopDrawing(e) {
     if (isDrawing) recordPoint(1);
     isDrawing = false;
     clearInterval(samplingInterval);
@@ -50,8 +65,17 @@ export function enableCanvasDrawing(canvas, drawDots = false) {
     if (!drawDots) ctx.beginPath();
   }
 
+  // Mouse events
+  canvas.addEventListener("mousedown", startDrawing);
+  canvas.addEventListener("mousemove", moveDrawing);
   canvas.addEventListener("mouseup", stopDrawing);
   canvas.addEventListener("mouseleave", stopDrawing);
+
+  // Touch events
+  canvas.addEventListener("touchstart", startDrawing, { passive: false });
+  canvas.addEventListener("touchmove", moveDrawing, { passive: false });
+  canvas.addEventListener("touchend", stopDrawing);
+  canvas.addEventListener("touchcancel", stopDrawing);
 
   // Return control methods
   return {
